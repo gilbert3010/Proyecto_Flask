2fe57682ccbe1ec63aea89733fba0eb073caa7e3
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-17 17:43:52 -0400

    proyecto-flask-database-version-1.0.3

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__) # Crear una instancia de Flask
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' # Configurar la URI de la base de datos (en este caso, SQLite)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Desactivar el seguimiento de modificaciones de objetos para mejorar el rendimiento
+
+
+
+db = SQLAlchemy(app) # crear una instancia de SQLAlchemy y pasarle la aplicación Flask
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    
+    def __repr__(self):
+        return f'<Article {self.title}>'
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+
+
+@app.route('/')# Decorador para definir la ruta de la página de inicio
 def home():
     return 'Hola, Flask!'
 
-from flask import Flask, request  # ← IMPORTANTE: importar request
+
 
 @app.route('/create-article', methods=['GET', 'POST'])
 def create_article():
     if request.method == 'POST':
-        title = request.form['title']      
-        content = request.form.get('content')  
-        return f'Artículo creado: {title} - {content}'
+        
+        title = request.form['title']
+        content = request.form.get('content')
+        
+        new_article = Article(title=title, content=content)
+        db.session.add(new_article)
+        db.session.commit()
+        
+        return f'Articulo creado {new_article.title}, contenido: {new_article.content}'
+        
+        
     return '''
 
         <form method = "POST" action="create-article">
