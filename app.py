da53d9b88afee497090c54c62d0c87692278b0b8
gilbert3010 <gilbert.meneses30@gmail.com>
2026-04-17 17:03:12 -0400

    proyecto-flask-html-version-1.0.2

--- app.py
+++ app.py
@@ -6,9 +6,27 @@ app = Flask(__name__)
 def home():
     return 'Hola, Flask!'
 
-@app.route('/create-article')
+from flask import Flask, request  # ← IMPORTANTE: importar request
+
+@app.route('/create-article', methods=['GET', 'POST'])
 def create_article():
-    return 'Aqui podemos crear un articulo'
+    if request.method == 'POST':
+        title = request.form['title']      
+        content = request.form.get('content')  
+        return f'Artículo creado: {title} - {content}'
+    return '''
+
+        <form method = "POST" action="create-article">
+            <label form='title'> Título del artículo: </label><br>
+            <input type='text' id='title' name='title'><br>
+            
+            <label form='content'> Contenido del artículo: </label><br>
+            <textarea id='content' name='content'></textarea><br>
+            
+            <input type='submit' value='Crear Artículo'>
+        </form>
+
+'''
 
 @app.route('/article/<int:article_id>')
 def view_article(article_id):
